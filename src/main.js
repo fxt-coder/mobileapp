972fd91a4bf8924a99100bf8d6ba7bd7b71a9303
傅雪涛 <1403664294@qq.com>
2020-07-24 10:52:09 +0800

    vant配置 rem配置

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'normalize.css'
+// 整合vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 注册vant
+Vue.use(Vant)
 Vue.config.productionTip = false
 // asdasd
 new Vue({
