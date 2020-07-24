52b8617f93f0da1a5eb6acb2fa5f0b8041f1f2d8
傅雪涛 <1403664294@qq.com>
2020-07-24 20:45:31 +0800

    tabbar整合

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,56 @@
 <template>
   <div class="app">
-    app
-    <i class="iconfont iconyanzhengma"></i>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
+    <router-view></router-view>
+    <van-tabbar
+      active-color="#E40137"
+      inactive-color="#B4B4BD"
+      v-model="active"
+      route
+      placeholder
+    >
+      <van-tabbar-item to="/Company" icon="home-o"
+        ><span>公司</span>
+        <template #icon>
+          <i class="iconfont iconicon_footbar_gs_nor"></i> </template
+      ></van-tabbar-item>
+      <van-tabbar-item to="/Question" icon="search"
+        ><span>刷题</span>
+        <template #icon>
+          <i class="iconfont iconicon_footbar_st_sel"></i> </template
+      ></van-tabbar-item>
+      <van-tabbar-item to="/Find" icon="friends-o"
+        ><span>发现</span>
+        <template #icon>
+          <i class="iconfont iconicon_footbar_fx_nor"></i> </template
+      ></van-tabbar-item>
+      <van-tabbar-item to="/My" icon="setting-o"
+        ><span>我的</span>
+        <template #icon>
+          <i class="iconfont iconicon_footbar_wode_sel"></i> </template
+      ></van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      active: 0
+    }
+  }
+}
 </script>
 
 <style lang="less">
 .app {
-  height: 100px;
-  width: 100px;
+  .iconfont {
+    font-size: 28px;
+  }
+  .van-tabbar-item--active {
+    color: @main-color;
+  }
+  .van-tabbar-item {
+    font-size: 10px;
+  }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Company from '@/views/Company/index.vue'
+import Find from '@/views/Find/index.vue'
+import My from '@/views/My/index.vue'
+import Question from '@/views/Question/index.vue'
 Vue.use(VueRouter)
 
-const routes = [
-]
-
 const router = new VueRouter({
-  routes
+  routes: [
+    {
+      path: '/Company',
+      component: Company
+
+    },
+    {
+      path: '/Find',
+      component: Find
+    },
+    {
+      path: '/My',
+      component: My
+    },
+    {
+      path: '/Question',
+      component: Question
+    }
+  ]
 })
 
 export default router
